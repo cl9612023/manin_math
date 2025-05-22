f6beddf517c213cc1efc73eb1d29d4d2414b1fd1
charlie_liang <cl9612023v2@gmail.com>
2025-05-22 22:53:30 +0800

    2025/05/22 change the vedio name and add the date

--- polygon.py
+++ polygon.py
@@ -1,5 +1,9 @@
 from manim import *
+from datetime import datetime
 
+today_date = datetime.now().strftime("%Y_%m_%d")
+
+config.output_file = f"Polygon_{today_date}.mp4"
 config.media_dir = "/media/sf_share/manim_learning"
 
 class Polygon(Scene):
