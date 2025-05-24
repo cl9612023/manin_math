2c01cfc4ce3e8f0d586946c1c8ad8c844c05a0d0
charlie_liang <cl9612023v2@gmail.com>
2025-05-24 22:58:38 +0800

    2025/05/25 change the background color to white

--- polygon.py
+++ polygon.py
@@ -8,6 +8,7 @@ config.media_dir = "/media/sf_share/manim_learning"
 
 class Polygon(Scene):
     def construct(self):
-        pentagon = RegularPolygon(n=5)
+        self.camera.background_color = WHITE #背景顏色
+        pentagon = RegularPolygon(n=5) #五邊形
         self.play(Create(pentagon))
         self.wait(1)
